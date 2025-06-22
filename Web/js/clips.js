6f9c5df00e33607135124399a4da430a01a0b993
EL SUKARDOOOOOOOOO <60936466+suckmyleg@users.noreply.github.com>
2025-06-22 23:31:16 +0200

    Update clips.js

--- Web/js/clips.js
+++ Web/js/clips.js
@@ -1,24 +1,24 @@
 let page = "clips";
 
 videos = [
-	["Igor se folla que?", "Videos/Saved_clips/Replay_1.mp4", "28/12/2022", ["jona", "bolli", "rive", "igor", "vendekebaps"]],
-	["Momento Jona", "Videos/Saved_clips/Replay_2.mp4", "28/12/2022", ["chino",  "jona", "cristian", "roobcrack", "bolli", "joel", "prostata"]],
-	["Callate Jona", "Videos/Saved_clips/Replay_3.mp4", "28/12/2022", ["roobcrack", "vendekebaps", "jona", "chino", "gamba", "prostata"]],
-	["Vendekebaps cocinando speed up", "Videos/Vende.mp4", "28/12/2022", ["vendekebaps"]],
-	["Juan bailando en el baño del dominos", "Videos/JuanBaile.mp4", "28/12/2022", ["suckmyleg"]],
-	["Roberto bailando beat saber", "Videos/robertoBailando.mp4", "28/12/2022", ["suckmyleg", "roberto"]],
-	["Roobcrack shaco", "Videos/RubenShaco2.mp4", "28/12/2022", ["roobcrack", "jona"]],
+	["Igor se folla que?", "Saved_clips/Replay_1.mp4", "28/12/2022", ["jona", "bolli", "rive", "igor", "vendekebaps"]],
+	["Momento Jona", "Saved_clips/Replay_2.mp4", "28/12/2022", ["chino",  "jona", "cristian", "roobcrack", "bolli", "joel", "prostata"]],
+	["Callate Jona", "Saved_clips/Replay_3.mp4", "28/12/2022", ["roobcrack", "vendekebaps", "jona", "chino", "gamba", "prostata"]],
+	["Vendekebaps cocinando speed up", "Vende.mp4", "28/12/2022", ["vendekebaps"]],
+	["Juan bailando en el baño del dominos", "JuanBaile.mp4", "28/12/2022", ["suckmyleg"]],
+	["Roberto bailando beat saber", "robertoBailando.mp4", "28/12/2022", ["suckmyleg", "roberto"]],
+	["Roobcrack shaco", "RubenShaco2.mp4", "28/12/2022", ["roobcrack", "jona"]],
 	//["Ruben y joeri csgo spin", "RJCJH.mp4", "04/11/2019"],
-	["Roberto basadisimo", "Videos/robertoBasado.mp4", "07/06/2022", ["roberto", "cristian"]],
-	["Rive pistolero disco", "Videos/RiveDisco1.mp4", "28/04/2022", ["rive", "gamba", "morgan", "humano", "rico", "roberto", "jona"]],
-	["Rico 07", "Videos/RicoQueDice.mp4", "/04/2022", ["rico"]],
-	["Rico buen setup", "Videos/RicoScream.mp4", "16/02/2022", ["rico", "joeri", "roberto", "rive", "gamba"]],
-	["Joeri siendo racista", "Videos/Clips/JoeriRacista.mp4", "13/08/2020", ["joeri", "david", "rive"]],
+	["Roberto basadisimo", "robertoBasado.mp4", "07/06/2022", ["roberto", "cristian"]],
+	["Rive pistolero disco", "RiveDisco1.mp4", "28/04/2022", ["rive", "gamba", "morgan", "humano", "rico", "roberto", "jona"]],
+	["Rico 07", "RicoQueDice.mp4", "/04/2022", ["rico"]],
+	["Rico buen setup", "RicoScream.mp4", "16/02/2022", ["rico", "joeri", "roberto", "rive", "gamba"]],
+	["Joeri siendo racista", "Clips/JoeriRacista.mp4", "13/08/2020", ["joeri", "david", "rive"]],
 	//["Rive tremendo tiro", "Clips/InGameReplay_2020.08.14-23.32.mp4", "14/08/2020", ["suckmyleg", "vendekebaps", "desi", "rive", "nacho"]],
 	//["No no no, me voy a hacer un colateral", "Clips/InGameReplay_2020.08.14-23.43.mp4", "14/08/2020", ["suckmyleg", "rive", "desi", "joeri", "morgan"]],
-	["Mas raro seria no comer pollas", "Videos/Clips/mas_raro_seria_no_comer_pollas.mp4", "15/08/2020", ["suckmyleg", "rive", "nacho"]], 
-	["Hablemos de comer pollas", "Videos/Clips/a1.mp4", "15/08/2020", ["rive", "suckmyleg", "roberto", "nacho"]],
-	["Nacho minimintu?", "Videos/Clips/a2.mp4", "17/08/2020", ["nacho", "humano", "suckmyleg"]]
+	["Mas raro seria no comer pollas", "Clips/mas_raro_seria_no_comer_pollas.mp4", "15/08/2020", ["suckmyleg", "rive", "nacho"]], 
+	["Hablemos de comer pollas", "Clips/a1.mp4", "15/08/2020", ["rive", "suckmyleg", "roberto", "nacho"]],
+	["Nacho minimintu?", "Clips/a2.mp4", "17/08/2020", ["nacho", "humano", "suckmyleg"]]
 ];
 
 let peopleShowing = [];
